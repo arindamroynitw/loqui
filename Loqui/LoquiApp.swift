120ffda8a16364efda04aec26a6e8b5f2865cb12
Arindam Roy <arindamroynitw@gmail.com>
2025-12-31 01:19:06 +0530

    Initial Commit

---
+++ Loqui/LoquiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoquiApp.swift
+//  Loqui
+//
+//  Created by Arindam Roy on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LoquiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
